8fb1c4ad4ee0159fe5455856dd3deae5ca9c4a6f
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-03 00:06:38 +0100

    Created function to add divs to the page

---
+++ script.js
@@ -0,0 +1,10 @@
+const easGrid = document.querySelector('#eas-grid');
+
+function createDiv() {
+    const div = document.createElement('div')
+    easGrid.append(div);
+    return;
+}
+
+createDiv()
+createDiv()
