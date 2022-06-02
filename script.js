d6d57c5e009152f6dc658aa5b13ca26a6bdeea12
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-03 00:25:13 +0100

    Added a loop to the div function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const easGrid = document.querySelector('#eas-grid');
 
 function createDiv() {
-    const div = document.createElement('div')
-    easGrid.append(div);
-    return;
+    for (let i = 0; i < 16; i++) {
+        const div = document.createElement('div')
+        easGrid.append(div);
+    }
 }
 
-createDiv()
 createDiv()
