299c2b48f3eb4e049ff4b193b89d28ac2caa93f1
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-03 18:44:13 +0100

    Hover effect now changes the color on hover

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ const easGrid = document.querySelector('#eas-grid');
 
 easGrid.addEventListener('mouseover', e => {
     if(e.target.matches('div')) {
-        console.log();
+        e.target.style.backgroundColor = 'red';
     }
-})
-
+});
 
 function createDiv() {
     for (let i = 0; i < 16; i++) {
         let div = document.createElement('div');
-        // div.classList.add('.grid-item')
         easGrid.append(div);
-        console.log()
     }
 }
 
